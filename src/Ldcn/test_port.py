fa205be5a24d3618548102802b65f781a6239b04
Craig Versek <cversek@o2ab.(none)>
2013-04-19 19:57:35 -0400

    testing code

--- src/Ldcn/test_port.py
+++ src/Ldcn/test_port.py
@@ -2,17 +2,17 @@ import time
 from ctypes import *
 ldcn = cdll.LoadLibrary("./libLdcn.so")
 
-port = c_char_p("/dev/ttyUSB0")
+port = c_char_p("/dev/ttyUSB2")
 baudrate = c_long(9600)
 
-#handle = ldcn.SioOpen(port,baudrate)
+handle = ldcn.SioOpen(port,baudrate)
 
-#print "handle =", handle
+print "handle =", handle
 
-#test = "Hello! ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890 abcdefghijklmnopqrstuvwxyz !@#$%^&*()"
-#n = c_long(len(test))
-#success = ldcn.SioPutChars(handle, c_char_p(test),n)
+test = "Hello! ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890 abcdefghijklmnopqrstuvwxyz !@#$%^&*()"
+n = c_long(len(test))
+success = ldcn.SioPutChars(handle, c_char_p(test),n)
 #print success
-##time.sleep(0.1)
+time.sleep(0.1)
 
 #ldcn.LdcnHardReset()
